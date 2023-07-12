eebe591db21f8208821461199cc3e7e2d95569b4
Abhay Kumar Singh <abhayanuj22@gmail.com>
2023-07-12 23:59:05 +0530

    movie_application

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const cors = require('cors');
 const mongo = require('./db.js');
+const helmet = require('helmet');
+const  xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
 mongo();
 app.use(cors());
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
 const port = 5000;
 app.use(express.json());
 const routes=require('./Routes.js')
